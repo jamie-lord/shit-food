b44e5ddf1105a5b8bc1dc20c6e8fdac4d7c23fc7
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-10-04 16:49:57 +0100

    Added cutlery svg

--- ShitFood.Web/js/get-restaurants.js
+++ ShitFood.Web/js/get-restaurants.js
@@ -1,6 +1,13 @@
 var axios = require('axios');
 
 function getRestaurants() {
+  var myIcon = L.icon({
+    iconUrl: '../img/markers/cutlery.svg',
+    iconSize: [36, 36], // size of the icon
+    iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
+    popupAnchor: [18, -3] // point from which the popup should open relative to the iconAnchor
+  });
+
   axios.get('https://shitfoodapi.azurewebsites.net/api/getshit?lat=52.9548&lng=-1.1581')
       .then(function (response) {
         for (var i = 0; i < response.length; i++) {
