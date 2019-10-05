31d0c7a90e30ae2689970e111a1211bfcc5497aa
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-10-05 21:18:52 +0100

    PR changes

--- ShitFood.Web/js/get-restaurants.js
+++ ShitFood.Web/js/get-restaurants.js
@@ -2,7 +2,7 @@ var axios = require('axios');
 
 function getRestaurants() {
   var cutleryIcon = L.icon({
-    iconUrl: '../img/markers/cutlery.svg',
+    iconUrl: './img/markers/cutlery.svg',
     iconSize: [36, 36], // size of the icon
     iconAnchor: [0, 0], // point of the icon which will correspond to marker's location
     popupAnchor: [18, -3] // point from which the popup should open relative to the iconAnchor
@@ -10,21 +10,21 @@ function getRestaurants() {
 
   axios.get('https://shitfoodapi.azurewebsites.net/api/getshit?lat=52.9548&lng=-1.1581')
       .then(function (response) {
-        for (var i = 0; i < response.length; i++) {
+        for (var i = 0; i < response.data.length; i++) {
           L.marker(
               [
-                response[i].lat,
-                response[i].lng
+                response.data[i].lat,
+                response.data[i].lng
               ],
               {
                 icon: cutleryIcon,
-                alt: response[i].name,
-                title: response[i].name,
+                alt: response.data[i].name,
+                title: response.data[i].name,
               }
           ).addTo(mymap)
-              .bindPopup(response[i].name + ' : Food Hygiene Rating - ' + response[i].foodHygieneRating);
+              .bindPopup(response.data[i].name + ' : Food Hygiene Rating - ' + response.data[i].foodHygieneRating);
         }
-        console.log(response);
+        console.log(response.data);
       })
       .catch(function (error) {
         console.log(error);
