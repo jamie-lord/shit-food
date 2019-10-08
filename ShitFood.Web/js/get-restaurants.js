28b1bed9a6c3ec6535428726adfa94c1a250ecce
Jamie Lord <jamie@lord.technology>
2019-10-08 20:44:11 +0100

    Fix cutlery icons

--- ShitFood.Web/js/get-restaurants.js
+++ ShitFood.Web/js/get-restaurants.js
@@ -1,71 +1,54 @@
-var axios = require('axios');
-
 function getRestaurants() {
-  var cutleryIcon = L.icon({
-    iconUrl: './img/markers/cutlery.svg',
-    iconSize: [36, 36],
-    iconAnchor: [0, 0],
-    popupAnchor: [18, -3]
-  });
-
-  var cutleryIconYellow = L.icon({
-    iconUrl: './img/markers/cutlery-yellow.svg',
-    iconSize: [36, 36],
-    iconAnchor: [0, 0],
-    popupAnchor: [18, -3]
-  });
-
-  var cutleryIconOrange = L.icon({
-    iconUrl: './img/markers/cutlery-orange.svg',
-    iconSize: [36, 36],
-    iconAnchor: [0, 0],
-    popupAnchor: [18, -3]
-  });
-
-  var cutleryIconRed = L.icon({
-    iconUrl: './img/markers/cutlery-red.svg',
-    iconSize: [36, 36],
-    iconAnchor: [0, 0],
-    popupAnchor: [18, -3]
-  });
-
-  axios.get('https://shitfoodapi.azurewebsites.net/api/getshit?lat=52.9548&lng=-1.1581')
-      .then(function (response) {
-        for (var i = 0; i < response.data.length; i++) {
-          L.marker(
-              [
-                response.data[i].lat,
-                response.data[i].lng
-              ],
-              {
-                icon: cutleryIconColour(response.data[i].foodHygieneRating),
-                alt: response.data[i].name,
-                title: response.data[i].name,
-              }
-          ).addTo(mymap)
-              .bindPopup(response.data[i].name + ' : Food Hygiene Rating - ' + response.data[i].foodHygieneRating);
-        }
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      })
-      .finally(function () {
-        alert('Enjoy your food')
-      });
+  axios
+    .get("http://localhost:7071/api/getshit?lat=52.9548&lng=-1.1581")
+    .then(function(response) {
+      for (var i = 0; i < response.data.length; i++) {
+        L.marker([response.data[i].lat, response.data[i].lng], {
+          icon: cutleryIconColour(response.data[i].foodHygieneRating),
+          alt: response.data[i].name,
+          title: response.data[i].name
+        })
+          .addTo(mymap)
+          .bindPopup(
+            response.data[i].name +
+              " : Food Hygiene Rating - " +
+              response.data[i].foodHygieneRating
+          );
+      }
+      console.log(response.data);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
 }
 
 function cutleryIconColour(rating) {
-  if (rating === 2) {
-    return cutleryIconYellow
+  if (rating === "2") {
+    return L.icon({
+      iconUrl: "./img/markers/cutlery-yellow.svg",
+      iconSize: [36, 36],
+      iconAnchor: [0, 0],
+      popupAnchor: [18, -3]
+    });
+  } else if (rating === "1") {
+    return L.icon({
+      iconUrl: "./img/markers/cutlery-orange.svg",
+      iconSize: [36, 36],
+      iconAnchor: [0, 0],
+      popupAnchor: [18, -3]
+    });
+  } else if (rating === "0") {
+    return L.icon({
+      iconUrl: "./img/markers/cutlery-red.svg",
+      iconSize: [36, 36],
+      iconAnchor: [0, 0],
+      popupAnchor: [18, -3]
+    });
   }
-  else if (rating === 1) {
-    return cutleryIconOrange
-  }
-  else if (rating === 0) {
-    return cutleryIconRed
-  }
-  else {
-    return cutleryIcon
-  }
+  return L.icon({
+    iconUrl: "./img/markers/cutlery.svg",
+    iconSize: [36, 36],
+    iconAnchor: [0, 0],
+    popupAnchor: [18, -3]
+  });
 }
