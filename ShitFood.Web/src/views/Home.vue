74bf6a95d0335880ce8b99459d81f57c0092e23d
Jack Domleo <jack.domleo@mitrefinch.co.uk>
2019-10-30 16:04:37 +0000

    Call API

--- ShitFood.Web/src/views/Home.vue
+++ ShitFood.Web/src/views/Home.vue
@@ -29,6 +29,7 @@ import {
 } from "vue-property-decorator";
 import L from "leaflet";
 import { LMap, LTileLayer, LMarker } from "vue2-leaflet";
+import Axios from "axios";
 
 @Component({
   components: { LMap, LTileLayer, LMarker }
@@ -43,6 +44,13 @@ export default class Home extends Vue {
     popupAnchor: [34, 0] // point from which the popup should open relative to the iconAnchor
   });
 
+  private cutleryIcon: L.Icon = new L.Icon({
+    iconUrl: "img/markers/cutlery.svg",
+    iconSize: [36, 36], // size of the icon
+    iconAnchor: [18, 18], // point of the icon which will correspond to marker's location
+    popupAnchor: [34, 0] // point from which the popup should open relative to the iconAnchor
+  });
+
   private getPosition() {
     navigator.geolocation.getCurrentPosition(position => {
       this.myPosition.lat = position.coords.latitude;
@@ -51,8 +59,25 @@ export default class Home extends Vue {
     });
   }
 
+  private getKillerRestaurants() {
+    var url: string;
+    navigator.geolocation.getCurrentPosition(position => {
+      url = "https://shitfoodapi.azurewebsites.net/api/getshit?lat=" + position.coords.latitude.toString() + "&lng" + position.coords.longitude.toString()
+    });
+    Axios.get(url).then(function (response) {
+      for (var i = 0; i < response.data.length ; i++) {
+        L.marker([response.data[i].lat, response.data[i].lng], {
+          icon: this.cutleryIcon,
+          alt: response.data[i].name,
+          title: response.data[i].name
+        })
+      }
+    })
+  }
+
   private async created() {
     this.getPosition();
+    this.getKillerRestaurants();
   }
 }
 </script>
